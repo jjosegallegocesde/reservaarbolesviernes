1b57f410d2ee0525379ee99ff9533a45086aae5f
fenix2902 <alexandercalleagudelo@gmail.com>
2023-11-09 18:46:21 -0500

    se realiza la forma de crear tablas

--- analysis/analisisUsuarios.py
+++ analysis/analisisUsuarios.py
@@ -1,5 +1,15 @@
 from data.ListaUsuarios import usuarios
 from helpers.crearCSV import crearCSVUsuarios
+from helpers.crearHTML import crearTabla
+
+import pandas as pd
 
 #Usamos la función crear CSVUsuarios
 crearCSVUsuarios(usuarios,'BDUsuarios.csv')
+
+# creando un dataframe desde una CSV
+dataFrameUsuarios = pd.read_csv('data/BDUsuarios.csv')
+print(dataFrameUsuarios)
+
+# convertir el DF en Tabla html
+crearTabla(dataFrameUsuarios,'usuarios')
