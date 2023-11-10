1882ca54ea4bf8b2d57c1cc8987f6a9aafa781b2
German Monroy <evelinlamejor2004@gmail.com>
2023-11-10 10:04:52 -0500

    se elimina carpeta

--- analysis/analisisRefrijerio.py
+++ analysis/analisisRefrijerio.py
@@ -1,4 +1,15 @@
 from data.refrigerios import refrijerios
 from helpers.crearCSV import crearCSVRefrigerios
+from helpers.crearHTML import crearTabla
 
-crearCSVRefrigerios(refrijerios,'BDRefrigerios')
+import pandas as pd
+
+#Usamos la función crear CSVRefrigerios
+crearCSVRefrigerios(refrijerios,'BDRefrigerios.csv')
+
+# creando un dataframe desde una CSV
+dataFrameRefrigerios = pd.read_csv('data/BDRefrigerios.csv')
+print(dataFrameRefrigerios)
+
+# convertir el DF en Tabla html
+crearTabla(dataFrameRefrigerios,'refrigerios')
